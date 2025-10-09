f3f23d1218008ee41a8f08aeb00db293708c0ef6
Anjaneyulu <127377390+Nerellaanjaneyulu@users.noreply.github.com>
2025-10-09 10:36:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
 region = "us-east-1"
 }
 resource "aws_instance" "one" {
-count           = 2
+  for_each      = toset(["dev-server", "test-server", "prod-server"])
   ami           = "ami-052064a798f08f0d3"
   instance_type = "t3.micro"
   tags = {
-    Name = "Anjaneyulu"
+    Name = "${each.key}"
   }
 }

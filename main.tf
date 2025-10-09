8cf6fb89c66efda62dc50e1fe32fdbb0467aa4a3
Anjaneyulu <127377390+Nerellaanjaneyulu@users.noreply.github.com>
2025-10-09 10:51:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "us-east-1"
 }
 resource "aws_instance" "one" {
-  for_each      = toset(["dev-server", "test-server", "prod-server"])
+  for_each      = toset(["dev-server", "test-server", "prod-server", "tech-server"])
   ami           = "ami-052064a798f08f0d3"
   instance_type = "t3.micro"
   tags = {

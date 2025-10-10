acd5df959fed2a3f7ad1e45d415fb7c55c7bd545
Anjaneyulu <127377390+Nerellaanjaneyulu@users.noreply.github.com>
2025-10-10 22:12:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 provider "aws" {
-    region = "us-east-1"
-  
+  region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-    count = length(var.instance_count)
-    ami = "ami-052064a798f08f0d3"
-    instance_type = var.instance_type[count.index]
-    tags = {
-        Name = var.instance_name[count.index]
-    }
+  count         = length(var.instance_type)
+  ami           = "ami-052064a798f08f0d3"
+  instance_type = var.instance_type[count.index]
+  tags = {
+    Name = var.instance_name[count.index]
+  }
 }
 
 
 variable "instance_type" {
-    default = ["t3.micro", "t3.small"]
+  default = ["t3.micro", "t3.small",]
 }
 
 variable "instance_name" {
-  default = ["dev-server", "test-server"]
+  default = ["dev-server", "test-server",]
 }
+

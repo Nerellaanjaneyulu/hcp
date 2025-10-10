1726cd1b0127019f36b57289fef77736c2ce242b
Anjaneyulu <127377390+Nerellaanjaneyulu@users.noreply.github.com>
2025-10-10 21:59:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,22 @@
 provider "aws" {
-region = "us-east-1"
+    region = "us-east-1"
+  
 }
-resource "aws_instance" "one" {
-  for_each      = toset(["dev-server", "test-server", "prod-server", ])
-  ami           = "ami-052064a798f08f0d3"
-  instance_type = "t3.micro"
-  tags = {
-    Name = "${each.key}"
-  }
+
+resource "aws_instance" "name" {
+    count = length(var.instance_count)
+    ami = "ami-052064a798f08f0d3"
+    instance_type = var.instance_type[count.index]
+    tags = {
+        Name = var.instance_name[count.index]
+    }
+}
+
+
+variable "instance_type" {
+    default = ["t3.micro", "t3.small"]
+}
+
+variable "instance_name" {
+  default = ["dev-server", "test-server"]
 }
